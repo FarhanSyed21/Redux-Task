b7e10995150539f6880a3b26844bdbd172199468
Syed Farhan <100605909+FarhanSyed21@users.noreply.github.com>
2023-08-07 12:14:33 +0530

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -3,13 +3,24 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
-      counter: state.counter + 1
+      counter: state.counter + 2
     };
   }
 
   if (action.type === "decrement") {
     return {
-      counter: state.counter - 1
+      counter: state.counter - 2
+    };
+  }
+
+  if (action.type === "increment5") {
+    return {
+      counter: state.counter + 5
+    };
+  }
+  if (action.type === "decrement5") {
+    return {
+      counter: state.counter - 5
     };
   }
 
