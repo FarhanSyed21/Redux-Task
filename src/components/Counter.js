83ad584dd035e97e890730905f17a0525241e055
Syed Farhan <100605909+FarhanSyed21@users.noreply.github.com>
2023-08-07 12:14:58 +0530

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,14 +12,22 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+  const incrementByHandler = () => {
+    dispatch({ type: "increment5" });
+  };
+  const decrementByHandler = () => {
+    dispatch({ type: "decrement5" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>IncrementBy2</button>
+        <button onClick={decrementHandler}>DecrementBy2</button>
+        <button onClick={incrementByHandler}>IncrementBy5</button>
+        <button onClick={decrementByHandler}>DecrementBy5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
